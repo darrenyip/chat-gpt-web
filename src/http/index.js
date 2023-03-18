8695443a89b31de127b77767b2f8a2e35b3c2aca
zhangyu ye <darrenyipzy@gmail.com>
2023-03-18 19:36:51 +0800

    udpate key

--- src/http/index.js
+++ src/http/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL: 'https://www.aiworksfine.com',
   headers: {
-    Authorization: 'Bearer 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',
+    Authorization: 'Bearer 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',
     'Content-Type': 'application/json'
   }
 })
