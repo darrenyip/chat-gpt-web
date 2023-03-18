25d277162cac5489eec5b389797e8785fca8b6c9
zhangyu ye <darrenyipzy@gmail.com>
2023-03-18 19:39:28 +0800

    qqq

--- src/http/index.js
+++ src/http/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL: 'https://www.aiworksfine.com',
   headers: {
-    Authorization: 'Bearer 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',
+    Authorization: 'Bearer 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',
     'Content-Type': 'application/json'
   }
 })
